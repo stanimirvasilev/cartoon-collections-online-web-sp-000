5a49eff423293f4547262f28175ae856a17aa7b2
Stanimir Vasilev <stanimirvasilev@yahoo.com>
2020-02-18 11:39:31 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,6 +15,6 @@ def long_planeteer_calls(calls)
 end
 
 def find_the_cheese (array)
-   cheese_types = ["cheddar", "gouda", "camembert"]
+   learn
   array.find { |a| cheese_types.include?(a) }
 end
